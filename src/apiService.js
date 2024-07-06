f619573e487d154d988ea2bdd3c682a14e411dd4
kj3004 <137755543+kj3004@users.noreply.github.com>
2024-07-06 16:34:52 +0530

    Dispatcher, Collector frontends and hadnling

--- src/apiService.js
+++ src/apiService.js
@@ -5,6 +5,8 @@ const API_URL = 'http://localhost:3001'; // Update with your backend URL
 const apiService = {
   register: (data) => axios.post(`${API_URL}/admin/register`, data),
   login: (data) => axios.post(`${API_URL}/admin/login`, data),
+  dispatcherLogin: (data) => axios.post(`${API_URL}/dispatcher/login`, data),
+  collectorLogin: (data) => axios.post(`${API_URL}/collector/login`, data),
 };
 
 export default apiService;
